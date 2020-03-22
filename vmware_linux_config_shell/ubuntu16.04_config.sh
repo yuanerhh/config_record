518d437caa86f956046c6264a03bb57d86784052
zhaosj1991 <zhaosj_1991@163.com>
2020-03-22 12:35:58 -0700

    use [echo | sudo tee] instead of [echo and sudo copy file].

--- vmware_linux_config_shell/ubuntu16.04_config.sh
+++ vmware_linux_config_shell/ubuntu16.04_config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd ~
 sudo cp -f /etc/apt/sources.list /etc/apt/sources.list.ori
 echo '''deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted universe multiverse
@@ -10,21 +9,12 @@ deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-backports main restricte
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
-
 # 预发布软件源，不建议启用
 # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse''' > sources.list.tsinghua
-
-sudo cp sources.list.tsinghua /etc/apt/
-sudo cp -f /etc/apt/sources.list.tsinghua /etc/apt/sources.list
-rm sources.list.tsinghua
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse''' | sudo tee /etc/apt/sources.list
 
 sudo apt-get update
 sudo apt-get install -y git vim ssh samba gcc g++ make
-
-cd ~
-mkdir git_folder && cd git_folder
-git clone https://github.com/zhaosj1991/linux_document.git
 
 cd ~
 mkdir share
@@ -37,10 +27,22 @@ echo '''[share]
    guest ok = yes
    create mask = 0700
    writable = yes
-   valid users = zhaosj''' >> smb.conf
-sudo cp -f smb.conf /etc/samba/smb.conf
-rm smb.conf
+   valid users = zhaosj''' | sudo tee -a /etc/samba/smb.conf
 
-cd ~
-sudo cp -f ~/git_folder/linux_document/network_conf/interfaces /etc/network/interfaces
+echo '''# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto ens33
+iface ens33 inet static
+address 192.168.150.131
+netmask 255.255.255.0
+gateway 192.168.150.2
+dns-nameserver 114.114.114.114''' | sudo tee /etc/network/interfaces
 sudo vi /etc/network/interfaces
